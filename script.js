fa1fb0c96def9b40532f18bdb446289833480b5a
MirzaShahzadSaleem <79744069+MirzaShahzadSaleem@users.noreply.github.com>
2022-09-08 15:46:57 +0500

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const toggle = document.querySelector('.toggle');
 const skipButtons = document.querySelectorAll('[data-skip]');
 const ranges = document.querySelector('.player__slider');
 
-console.log({skipButtons, ranges, video})
 
+//console.log({skipButtons, ranges, video})
+// console.log(ranges.value);
 /* Build our functions*/
 function togglePlay(){
     const method = video.paused ? 'play' : 'pause';
@@ -20,16 +21,26 @@ function updateButton() {
     toggle.textContent = icon;
 }
 function skip() {
-    console.log(this.dataset.skip);
+   // console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate() {
-    video[this.value] = this.name;
+// function handleRangeUpdate() {
+//     video.playbackRate[this.value] = this.value;
+//      console.log('range working');
+//     //video.playbackRate = 2;
+// }
+
+function playbackRate() {
+    video[this.name] = this.value;
+    video.playbackRate = video[this.name];
+
+    console.log(video[this.name]);
 }
+
 function handleProgress() {
-    const percent =(video.currentTime / video.duration)
-    progressBar.getElementsByClassName.flexBasis = `${percent}%`;
+    const percent =(video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${percent}%`;
 }
 
 /* Hook up the event listeners */
@@ -37,8 +48,14 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
+ranges.addEventListener('change',playbackRate);
+//ranges.addEventListener('mousemove', handleRangeUpdate);
 
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
-ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+// ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+// ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+
+// <input type="range" name="volume" class="player__slider" min="0" max="1" step="0.2" value="1">
